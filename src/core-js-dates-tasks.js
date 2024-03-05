c40282535b883f3723d35b9e666293566ef734bc
Salt-Upon-Wounds <kiw197204@gmail.com>
2024-03-05 23:29:42 +0300

    feat: 5 tasks done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -96,8 +96,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (
+    1 +
+    Math.abs((new Date(dateStart) - new Date(dateEnd)) / 60 / 1000 / 60 / 24)
+  );
 }
 
 /**
